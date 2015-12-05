2220855dad4faf7eb15b43e377871200751f60f7
not-much-io <kristo.koert@gmail.com>
2015-12-05 20:30:32 +0200

    done

--- hello-world/repl.clj
+++ hello-world/repl.clj
@@ -3,10 +3,10 @@
 (require 'cljs.repl.browser)
 
 (cljs.build.api/build "src"
-                      {:main 'hello-world.core
-                       :output-to "out/main.js"
-                       :verbose true})
+  {:main 'hello-world.core
+   :output-to "out/main.js"
+   :verbose true})
 
 (cljs.repl/repl (cljs.repl.browser/repl-env)
-                :watch "src"
-                :output-dir "out")
+  :watch "src"
+  :output-dir "out")
